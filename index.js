215769af7ef14fcc2cfc94e6215213ea3ca9db87
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-09-23 16:56:22 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    const scuberHeadquarters = 42;
+    if (pickUpLocation >= scuberHeadquarters) {
+        return pickUpLocation - scuberHeadquarters;
+    } else {
+    return scuberHeadquarters - pickUpLocation;
+    }
+}
+function distanceFromHqInFeet(pickUpLocation) {
+    const scuberHeadquarters = 42;
+    if (pickUpLocation >= scuberHeadquarters) {
+        return (pickUpLocation - scuberHeadquarters) * 264;
+    } else {
+    return (scuberHeadquarters - pickUpLocation) * 264;
+    }
+}
+function distanceTravelledInFeet(start, destination) {
+    if (destination >= start) {
+        return (destination - start) * 264;
+        } else {
+    return (start - destination) * 264;
+    }
+  }
+ 
+function calculatesFarePrice(start, destination) {
+    if (destination >= start) {
+        const feets = (destination - start) * 264;
+        if (feets <= 400) {
+            return 0;
+        } else if (feets > 400 && feets <= 2000) {
+            return 2.56;
+        } else if (feets > 2000 && feets <= 2500) {
+            return 25;
+        } else {
+            return "cannot travel that far";
+        }
+    }
+
+    else {
+        const feets = (start - destination) * 264;
+        if (feets <= 400) {
+            return 0;
+        } else if (feets > 400 && feets <= 2000) {
+            return 2.56;
+        } else if (feets > 2000 && feets <= 2500) {
+            return 25;
+        } else {
+            return "cannot travel that far";
+        }
+    }
+}
